600cc4111afc6fa36d625e5af8afe496e35f057d
Hamilton <hamilton@hts-net.com.br>
2023-08-26 20:28:00 -0300

    first commit

---
+++ demo.js
@@ -0,0 +1,30 @@
+import {OpenAI} from 'langchain/llms/openai';
+import { PromptTemplate } from "langchain/prompts";
+import { LLMChain } from "langchain/chains";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const template = 'What would be a good company nome for a company that makes {product}?'
+const promptTemplate = new PromptTemplate({
+    template: template,
+    inputVariables: ['product'],
+});
+
+const formattedPrompt = await promptTemplate.format({
+        product: "Colorful socks",
+});
+
+const model = new OpenAI({
+    temperature: 0.9
+});
+
+const chain = new LLMChain({
+    llm: model,
+    prompt: promptTemplate,
+})
+
+const res = await chain.call({
+    product: "colorful socks",
+});
+
+console.log(res);
